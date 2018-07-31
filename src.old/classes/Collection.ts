3d7350baf9388221d0a2ed5c2d2332b9b015b804
rosostlato <eduardo.rosostolato@gmail.com>
2018-07-31 18:11:07 -0300

    remove old files

--- src.old/classes/Collection.ts
+++
@@ -1,5 +0,0 @@
-export class Collection<T> extends Array<T> {
-  constructor (items: T[]) {
-    super(...items);
-  }
-}
--- src.old/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './classes/restBase';
-export * from './classes/restRoute';
-export * from './classes/restModel';
-export * from './classes/restModelCollection';
-export * from './decorators/restful';
